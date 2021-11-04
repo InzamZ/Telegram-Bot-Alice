325936acd92806e03f35f7a0c16af2eafa25dd2b
InzamZ <zhengyinzhen2001@icloud.com>
2021-11-04 21:19:22 +0800

    :hammer: Add the main script

---
+++ main.py
@@ -0,0 +1,22 @@
+from telegram.ext import Updater
+import logging
+
+with open("token.secret.me","r") as f:
+    TOKEN = f.read()
+
+updater = Updater(token=TOKEN, use_context=True)
+
+dispatcher = updater.dispatcher
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
+# check if the bot is working
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+from telegram.ext import CommandHandler
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+updater.start_polling()
