558e7c2e187d438f7740d1af5243fa0d3bcaf8a8
InzamZ <zhengyinzhen2001@icloud.com>
2021-11-06 00:06:09 +0800

    :hammer: 现在可以更新已存在的Deadline的信息

--- ModeCode/Deadline.py
+++ ModeCode/Deadline.py
@@ -1,4 +1,5 @@
 import json
+from json import encoder
 import random
 import datetime
 from telegram import ParseMode
@@ -10,9 +11,10 @@ now = datetime.datetime.now(timezone)
 def ddl_cmp(a):
     return datetime.datetime.fromisoformat(a['deadline'])
 
-def load_json():
+
+def load_json(DDLFilePath="./Data/ddl.json"):
     ddl_list = []
-    with open("ddl.json", "r", newline="") as f:
+    with open(DDLFilePath, "r", newline="", encoding="GBK") as f:
         ddl_dict = json.load(f)
         for x in ddl_dict:
             ddl_list.append(x)
@@ -20,31 +22,94 @@ def load_json():
     return ddl_list
 
 
-def ddl(update, context):
-    msg_str= "Here are the *TODO* list:\n"
-    ddl_list = load_json()
+def save_json(DDLFilePath="./Data/ddl.json", src=None):
+    with open(DDLFilePath, "w", encoding="GBK") as f:
+        f.write(json.dumps(src, indent=4, ensure_ascii=False))
+
+
+def get_ddl_msg(ddl_list):
+    cnt = 0
+    msg_str = "Here are the *TODO* list:\n"
+    msg_str += ("Now is *" + now.ctime() + "*\n")
     for x in ddl_list:
         item_datetime = datetime.datetime.fromisoformat(x['deadline'])
-        if item_datetime < now :
-            msg_str += ('~' + item_datetime.ctime() + " " + x['todo'] + "~\n")
+        if item_datetime < now:
+            msg_str += ('_' + "\# " + str(cnt) + " " + item_datetime.ctime() +
+                        " " + x['todo'] + "_\n")
         elif item_datetime > now + datetime.timedelta(days=7):
-            msg_str += (item_datetime.ctime() + " " + x['todo'] + "\n")
+            msg_str += ("\# " + str(cnt) + " " +
+                        item_datetime.ctime() + " " + x['todo'] + "\n")
         else:
-            msg_str += ("*" + item_datetime.ctime() + " " + x['todo'] + "*\n")
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             parse_mode=ParseMode.MARKDOWN_V2, text=msg_str)
+            msg_str += ("*" + "\# " + str(cnt) + " " + item_datetime.ctime() +
+                        " " + x['todo'] + "*\n")
+        cnt = cnt + 1
+    return msg_str
+
+
+def ddl(update, context):
+    ddl_list = load_json()
+    if (len(context.args) == 0):
+        print(get_ddl_msg(ddl_list))
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 parse_mode=ParseMode.MARKDOWN_V2, text=get_ddl_msg(ddl_list))
+    elif context.args[0] == "add":
+        print("add")
+    elif context.args[0] == "update":
+        if (len(context.args) != 4):
+            context.bot.send_message(
+                chat_id=update.effective_chat.id, text="Wrong number args!")
+#            msg_str += "{id} \n"
+#            msg_str += "Example: **\n"
+#            context.bot.send_message(chat_id=update.effective_chat.id,
+#                                     parse_mode=ParseMode.MARKDOWN_V2, text=msg_str)
+        elif (context.args[1].isdigit()):
+            update_id = int(context.args[1])
+            if (update_id >= len(ddl_list)):
+                context.bot.send_message(
+                    chat_id=update.effective_chat.id, text="Invilid args!")
+            else:
+                Exc = False
+                try:
+                    update_datetime = datetime.datetime.fromisoformat(
+                        context.args[3])
+                except Exception:
+                    Exc = True
+                    context.bot.send_message(
+                        chat_id=update.effective_chat.id, text=("Invalid isoformat string!" + context.args[3]))
+                finally:
+                    pass
+                if (Exc == False and (context.args[2] == "deadline" or context.args[2] == "todo")):
+                    items = ["deadline", "todo"]
+                    print(type(ddl_list[0]))
+                    if update_datetime.tzinfo == None:
+                        update_datetime = datetime.datetime.fromisoformat(
+                            update_datetime.isoformat()+"+08:00")
+                    temp = ddl_list[update_id]
+                    temp[context.args[2]] = update_datetime.isoformat()
+                    print(ddl_list)
+                    save_json(src=ddl_list)
+                else:
+                    context.bot.send_message(
+                        chat_id=update.effective_chat.id, text="Invilid args (3rd)! " + "'" + context.args[2] + "'")
+        else:
+            print("False")
+#        context.bot.send_message(chat_id=update.effective_chat.id,
+#                                 parse_mode=ParseMode.MARKDOWN_V2, text=get_ddl_msg(ddl_list))
+    elif context.args[0] == "finish":
+        print("finish")
+    else:
+        print("Invalid arguement!")
+
 
 if __name__ == "__main__":
-    msg_str= "Here are the *TODO* list:\n"
+    msg_str = "Here are the *TODO* list:\n"
     ddl_list = load_json()
     for x in ddl_list:
         item_datetime = datetime.datetime.fromisoformat(x['deadline'])
-        if item_datetime < now :
+        if item_datetime < now:
             msg_str += ('~' + item_datetime.ctime() + " " + x['todo'] + "~\n")
         elif item_datetime > now + datetime.timedelta(days=7):
             msg_str += (item_datetime.ctime() + " " + x['todo'] + "\n")
         else:
             msg_str += ("*" + item_datetime.ctime() + " " + x['todo'] + "*\n")
     print(msg_str)
-
-    
