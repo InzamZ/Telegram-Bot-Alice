be2ae2cb3f443f7a5347161c85adcf8a9bf5a835
ubuntu <ubuntu@localhost.localdomain>
2021-11-16 15:35:00 +0800

    :see_no_evil: update gitignore file

--- ModeCode/Icloud.py
+++ ModeCode/Icloud.py
@@ -1,14 +1,7 @@
 import json
 import pyicloud
 
-icloudapi = pyicloud.PyiCloudService('3028891035@qq.com')
-print(icloudapi.drive.dir())
+api = pyicloud.PyiCloudService('3028891035@qq.com')
+# print(icloudapi.drive.dir())
 
-f = icloudapi.drive['Telegram-Bot-Alice']['sleep.json']
-from shutil import copyfileobj
-with f.open(stream=True) as response:
-    with open(f.name, 'wb') as file_out:
-        copyfileobj(response.raw, file_out)
-        sleepinfo = str()
-        file_out.write(sleepinfo)
 
